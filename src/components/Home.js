b2fe676f2ab228e8833a1414c3c74cec365d6d16
Arshpreet Singh <arshpreetsingh@arshpreets-mbp.home>
2023-04-29 14:50:59 -0400

    Pushing some code changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a front end developer (and lifelong learner)!</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a passionate software developer having diverse portfolio of projects, courses and experience in various technologies. I'm currently looking for software engineering coops to learn new skills and expand and grow in my career. Currenly focused on building fast and responsive web applications</p>
         <div>
-            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-400 hover:border-pink-400'> View Work
+            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-400 hover:border-pink-400 cursor-pointer'> View Work
             <span className='group-hover:rotate-90 duration-300'>
             <HiArrowNarrowRight className='ml-3'/>
             </span>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import logo from "../assets/logo.png";
-import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook, FaLink, FaLinkedinIn} from "react-icons/fa";
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from "react-icons/fa";
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill} from "react-icons/bs";
 import {Link} from "react-scroll";
@@ -20,38 +20,38 @@ const Navbar = () => {
 
 
             <ul className='hidden md:flex'>
-                <li>
+                <li className='hover:scale-110 duration-200'>
                 <Link  activeClass="active" to="Home"  smooth={true} duration={500} >
                  Home
                 </Link>
                 </li>
-                <li>
+                <li className='hover:scale-110 duration-200'>
                 <Link  to="about"  smooth={true} duration={500} >
                  About
                 </Link>
                 </li>
-                <li>
+                <li className='hover:scale-110 duration-200'>
                 <Link  to="skills"  smooth={true} duration={500} >
                  Skills
                 </Link>
                 </li>
-                <li>
+                <li className='hover:scale-110 duration-200'>
                 <Link  to="work-ex"  smooth={true} duration={500} >
                  Work Experience
                 </Link>
                 </li>
-                <li> 
+                <li className='hover:scale-110 duration-200'> 
                 <Link  to="projects"  smooth={true} duration={500} >
                  Projects
                 </Link></li>
-                <li> 
+                <li className='hover:scale-110 duration-200'> 
                 <Link  to="contact"  smooth={true} duration={500} >
                  Contact
                 </Link></li>
             </ul>
         
         {/** Hamburger */}
-        <div onClick={handleClick} className='md:hidden z-10'> 
+        <div onClick={handleClick} className='cursor-pointer md:hidden z-10'> 
             {!nav ? <FaBars /> : <FaTimes />}
         </div>
         {/**Mobile Menu absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'*/}
@@ -110,7 +110,7 @@ const Navbar = () => {
                 </li>
                 <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300">
                     <a className="flex justify-between items-center w-full text-gray-200"
-                       href='/'>
+                       href='/' download={true}>
                         Resume <BsFillPersonLinesFill size={30} />
                     </a>
                 </li>
